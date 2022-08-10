4298327484348fdba2f6d43951bb7ab2c1e971d6
Justin Abellera <justin.c.abellera@gmail.com>
2022-08-09 19:41:31 -0700

    first push

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,9 +1,13 @@
 import React from 'react'
-
+import Form from './Form'
+import Todo from './Todo'
+import TodoList from './TodoList'
 const URL = 'http://localhost:9000/api/todos'
 
 export default class App extends React.Component {
   render() {
-    return null
+    return (
+      <Form />
+    )
   }
 }
